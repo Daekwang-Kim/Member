371a42b1c4ec8048bef372eeffd4397c193579e5
Daekwang <jirungi7@gmail.com>
2021-03-14 19:30:48 +0900

    first commit

---
+++ src/main/java/com/example/test/Controller/MemberController.java
@@ -0,0 +1,67 @@
+package com.example.test.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.test.Models.Member;
+import com.example.test.Service.MemberService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("memberTest")
+public class MemberController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<List<Member>> getAllmembers() {
+        List<Member> member = memberService.findAll();
+        return new ResponseEntity<List<Member>>(member, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{mbrNo}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Member> getMember(@PathVariable("mbrNo") Long mbrNo) {
+        Optional<Member> member = memberService.findById(mbrNo);
+        return new ResponseEntity<Member>(member.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{mbrNo}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Void> deleteMember(@PathVariable("mbrNo") Long mbrNo) {
+        memberService.deleteById(mbrNo);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(value = "/{mbrNo}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Member> updateMember(@PathVariable("mbrNo") Long mbrNo, Member member) {
+        memberService.updateById(mbrNo, member);
+        return new ResponseEntity<Member>(member, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Member> save(Member member) {
+        return new ResponseEntity<Member>(memberService.save(member), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/saveMember", method = RequestMethod.GET)
+    public ResponseEntity<Member> save(HttpServletRequest req, Member member) {
+        return new ResponseEntity<Member>(memberService.save(member), HttpStatus.OK);
+    }
+
+}
